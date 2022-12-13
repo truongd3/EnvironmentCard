14ab2d1ffc1d053be11379684493d8455c03085e
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2022-12-13 17:46:23 -0500

    Add files via upload

---
+++ WWDC2022_TruongDang/WWDC2022_TruongDangUITests/WWDC2022_TruongDangUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WWDC2022_TruongDangUITestsLaunchTests.swift
+//  WWDC2022_TruongDangUITests
+//
+//  Created by Đặng Đức Trường on 4/6/22.
+//
+
+import XCTest
+
+class WWDC2022_TruongDangUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
